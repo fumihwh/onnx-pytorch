bcf48ff29be9fc985a69a7bacac6b29a6ce32406
Wenhao Hu <fumihwh@gmail.com>
2021-05-07 14:07:06 +0800

    Add format check

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-pip install -r requirements.txt
-
 onnx_pytorch_dir="$PWD"
-pip install -e $onnx_pytorch_dir
 
-python --version
+yapf -ri $onnx_pytorch_dir/onnx_pytorch --exclude $onnx_pytorch_dir/onnx_model_maker/ops
+if [ $(git diff --cached --exit-code HEAD^ >/dev/null && (git ls-files --other --exclude-standard --directory | grep -c -v '/$')) ]; then
+  echo "yapf formatter check failed."
+  exit 1
+fi
 
 python -m pytest $onnx_pytorch_dir/onnx_pytorch/tests
