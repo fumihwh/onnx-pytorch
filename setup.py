d5181b1267f16d540881caec4bdce1103d0630d6
Wenhao Hu <fumihwh@gmail.com>
2021-11-23 15:10:42 +0800

    create python publish action (#42)
    * Bump up version to 0.1.4
    
    * Add cwd
    
    * Test publish package when push to release
    
    * Refactor setup.py
    
    * Remove push to release trigger for python-publish

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
+import os
+from typing import List
 from setuptools import setup, find_packages
 
-exec(open('onnx_pytorch/_version.py').read())
-version_str = __version__
 
-with open('requirements.txt') as f:
-    REQUIRED = f.read().splitlines()
+def _version() -> str:
+  from onnx_pytorch import _version
+  return _version.__version__
+
+
+def _parse_requirements() -> List[str]:
+  file_path = "requirements.txt"
+  if not os.path.exists(file_path):
+    file_path = "onnx_pytorch.egg-info/requires.txt"
+  with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                         file_path)) as f:
+    required = f.read().splitlines()
+  return required
+
 
 setup(name="onnx-pytorch",
-      version=version_str,
+      version=_version(),
       description="Convert ONNX to PyTorch code.",
       long_description=open("README.md").read(),
       long_description_content_type="text/markdown",
@@ -16,6 +28,6 @@ setup(name="onnx-pytorch",
       url="https://github.com/fumihwh/onnx-pytorch",
       packages=find_packages(),
       license="Apache 2.0",
-      install_requires=REQUIRED,
       scripts=["onnx_pytorch/code_gen.py"],
+      install_requires=_parse_requirements(),
       classifiers=["Programming Language :: Python :: 3"])
